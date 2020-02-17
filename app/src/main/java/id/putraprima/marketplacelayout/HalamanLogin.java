05b787feac92673a4ae06495ca5cc195bc8a19cd
sitiaminah07 <itmin0742@gmail.com>
2020-02-17 08:15:51 +0700

    Membuat halaman login, register, dan profil serta membuat dropdown pada country dan menambahkan ikon back

--- app/src/main/java/id/putraprima/marketplacelayout/HalamanLogin.java
+++ app/src/main/java/id/putraprima/marketplacelayout/HalamanLogin.java
@@ -6,17 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class HalamanLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_halaman_login);
     }
 
     public void postLogin(View view) {
-        Intent intent = new Intent(this, HalamanLogin.class);
+        Intent intent = new Intent(this, HalamanRegister.class);
         startActivity(intent);
     }
-
 }
--- app/src/main/java/id/putraprima/marketplacelayout/HalamanRegister.java
+++ app/src/main/java/id/putraprima/marketplacelayout/HalamanRegister.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class HalamanRegister extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_halaman_register);
     }
 
     public void postLogin(View view) {
@@ -19,4 +19,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void postNext(View view) {
+        Intent intent = new Intent(this, ProfilActivity.class);
+        startActivity(intent);
+    }
 }
